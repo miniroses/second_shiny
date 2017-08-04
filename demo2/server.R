35948ddcc7be98e078a177a5dfe06aef6edc3602
miniroses <1126496537@qq.com>
2017-08-04 12:34:15 +0800

    Add files via upload

---
+++ demo2/server.R
@@ -0,0 +1,28 @@
+library('shiny')
+options(shiny.maxRequestSize = 20000*1024^2)
+
+shinyServer(function(input, output,session) {
+  
+  output$mymap <- renderLeaflet({
+    req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    leaflet(df) %>%addProviderTiles("OpenStreetMap.Mapnik")%>% addCircleMarkers(
+      lng = ~longitude,
+      lat = ~latitude,
+      radius=3,
+      weight=2,
+      label= ~as.character(Bike_id)
+    )%>%addMarkers(clusterOptions = markerClusterOptions(freezeAtZoom = FALSE))
+    
+  })
+  
+  observeEvent(input$mymap_marker_click, {
+    leafletProxy("mymap", session) %>%
+      removeMarker(input$map1_marker_click$id)
+  })
+  
+})
